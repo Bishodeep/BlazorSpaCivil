4e15ee58065a0357f7fc2a6c7a4415613a9c78e8
bishodeep dahal <bishodeepdahal@Prakritis-Mac.local>
2025-04-29 22:08:26 +1000

    removes boilerplate codes

--- ValueMateApi/Program.cs
+++ ValueMateApi/Program.cs
@@ -40,25 +40,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
 app.MapPost("/login", (LoginRequest login, IAuthenticationSerivice authService) =>
 {
     var result = authService.Authenticate(login);
